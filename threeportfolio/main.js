6674e66e8e56028fab4610d2581cde76e6c59d34
samerjouhar <samerjouhar6@gmail.com>
2023-02-22 13:16:05 -0500

    added sliding animations

--- threeportfolio/main.js
+++ threeportfolio/main.js
@@ -15,6 +15,8 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 
+hiddenElements.forEach((el) => observer.observe(el));
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
